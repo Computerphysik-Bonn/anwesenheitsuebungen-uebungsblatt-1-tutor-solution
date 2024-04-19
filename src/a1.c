f448c60221a2127eabb91c13bc3a746b37d30ef3
Oliver Adam <oadam@hiskp.uni-bon.de>
2024-04-19 15:55:58 +0200

    Single file changes: added some first llines of code

---
+++ src/a1.c
@@ -0,0 +1,25 @@
+//If you have questions how several functions like fgets() work. Go into your terminal (supposedly the program "man" is installed) and type in: "man fgets()" to see full documentation and possible examples. 
+
+
+//If you open up the first dataset, dataset1. You see it is all ASCII text, so literal text we can see and interprete. The first line shows uf the number of lines the file has for data. 
+//We first will extract that number and use it as a iteration boundary for our for loop.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_LENGTH 8 
+
+int main()
+{
+	FILE *fp; //fp: File pointer
+	char buffer[BUFFER_LENGTH]; //Create buffer for later data storing
+	int lines; //Declare variable for the amount of lines to store
+	
+	//Get the amount of lines
+	fgets(buffer, BUFFER_LENGTH, fp); //Stores a line in a file specific to the fp. fgets() will read the line until it encounters a '\n' (new line)-character.
+	lines = atoi(buffer);	//Interestingly, this atoi() function can filter a '\n' character if it appears in the buffer.
+
+	fprintf(stdout, "%s", buffer);
+	fprintf(stdout, "%d", lines);
+	return 0;
+}
